a05f6ded9557fa34d7a86461306d9a07e1aa3319
Francisco Bendahan <franbendahan@gmail.com>
2025-10-24 09:03:05 -0300

    first commit

---
+++ src/blackjack/usecases/pedir-carta.js
@@ -0,0 +1,14 @@
+
+/**
+ * @param {Array<String>} deck Es un arreglo de string
+ * @returns {String} Retorna una carta del deck
+ */
+
+export const pedirCarta = ( deck ) => {
+    
+    if ( !deck || deck.length === 0 ) {
+        throw new Error ('No hay cartas en el deck');
+    }
+    const carta = deck.pop();
+    return carta;
+}
